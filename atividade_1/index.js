81821a23cba8e5ba4ae37a05caef549a7bf6cc6d
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-06-22 20:45:09 -0300

    Alterando variaveis

--- atividade_1/index.js
+++ atividade_1/index.js
@@ -1,4 +1,10 @@
 // ## Importe a biblioteca ##
+let rl = require("readline-sync");
+
+let numeroVoo = rl.question("Por favor informe o numero do seu Voo?");
+let letraFileira = rl.question("Por favor informe em qual fileira deseja sentar? (A, B, C, D ou F) ");
+
+console.log(`Seu voo é ${numeroVoo} Seu assento é na fileira ${letraFileira}`)
 
 
 
