09de466e6faac8301a8143247af1823c1a86782e
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-06-22 22:14:49 -0300

    Exercicio 3

--- atividade_3/index.js
+++ atividade_3/index.js
@@ -1,4 +1,21 @@
 // ## Importe a biblioteca ##
+let rl = require('readline-sync');
+
+let largura = rl.questionInt("Digite a Largura ");
+
+let comprimento = rl.questionInt("Digite o comprimento ");
+
+
+// questionInt converte para Inteiro
+//largura = Number(largura);
+//comprimento = Number(comprimento);
+
+//let calc = largura * comprimento
+
+comprimento = largura * comprimento
+
+console.log(`A área do quadrado é ${comprimento}`);
+
 
 
 
