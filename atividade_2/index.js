3c68c91f893c9b47194b538fad4b35dc3f5e1832
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-06-22 21:26:02 -0300

    Refaturando Código Operador ternario

--- atividade_2/index.js
+++ atividade_2/index.js
@@ -1,4 +1,16 @@
 // ## Importe a biblioteca ##
+let rl = require('readline-sync');
+
+let mascara = rl.question("Você está usando mascara ao sair de casa? (Sim ou Não)");
+
+let lavMao = rl.question("Está lavando as mãos frequentemente? (Sim ou Não)")
+
+//Operador ternario
+let Result = mascara === lavMao ? "Parabéns" : "Deixa de ser porco"; 
+console.log(Result)
+
+
+
 
 
 
