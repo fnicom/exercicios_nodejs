39410c06a73774319729f1af3c316d9d971470fa
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-10-18 10:10:50 -0300

    Update index.js

--- atividade_6/index.js
+++ atividade_6/index.js
@@ -1,5 +1,31 @@
 // ## Importe a biblioteca ##
 
+let rs = require('readline-sync');
 
+let  userUm = rs.question("Informe seu nome: \n");
+
+let  askUm = rs.question("Digite qualquer coisa: \n");
+
+let  userDois = rs.question("Informe seu nome: \n");
+
+let  askDois = rs.question("Digite qualquer coisa: \n");
+
+console.log(userUm + " voce digitou: " + askUm.length, "caracteres!");
+
+console.log(userDois + " voce digitou: " + askDois.length, "caracteres!");
+
+if (askUm.length > askDois.length) { 
+
+    console.log(`${userUm} digitou mais caracteres!`)
+    
+} if (askDois.length > askUm.length) {
+
+    console.log(`${userDois} digitou mais caracteres!`)
+
+} else  {
+
+    console.log("Os dois digitaram a mesma quantidade de caracteres!!! \n")
+
+} 
 
 // ## Faça o código ##
