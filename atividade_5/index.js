5dacd95f63230c30dd093ac0f73aedbda6f1f138
Francisco Nicomedes <52546292+fnicom@users.noreply.github.com>
2020-10-18 10:10:26 -0300

    Update index.js

--- atividade_5/index.js
+++ atividade_5/index.js
@@ -1,5 +1,9 @@
 // ## Importe a biblioteca ##
 
+let rs = require('readline-sync');
 
+let num = rs.question("Insira uma frase ou um nome: \n");
+
+console.log("Voce digitou: " + num.length, "caracteres!");
 
 // ## Faça o código ##
